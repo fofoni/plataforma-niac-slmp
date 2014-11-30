99503b7f95248dbfe4d3a08c724f26c6bcfbc63a
newworldtraveler@gmail.com <newworldtraveler@gmail.com@7746d04f-d786-4d59-b84b-d35678cca735>
2014-11-30 21:00:34 +0000

    AGORA SIM, o login vai pra tela certa :D

--- trunk/app/Controller/PagesController.php
+++ trunk/app/Controller/PagesController.php
@@ -38,7 +38,7 @@ class PagesController extends AppController {
 	public $uses = array();
 
 	public function index() {
-        return $this->redirect(array('action' => 'home'));
+        //return $this->redirect(array('action' => 'home'));
 	}
 
 /**
