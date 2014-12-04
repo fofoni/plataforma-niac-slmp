fb67f4027caba5315468fc607574b588274f3311
newworldtraveler@gmail.com <newworldtraveler@gmail.com@7746d04f-d786-4d59-b84b-d35678cca735>
2014-12-04 18:27:42 +0000

    Alteração do TODO e validações de Cliente.php

--- app/Model/Cliente.php
+++ app/Model/Cliente.php
@@ -39,6 +39,22 @@ class Cliente extends AppModel {
 				//'on' => 'create', // Limit validation to 'create' or 'update' operations
 			),
 		),
+		'cpf' => array(
+			'rule' => array('minLength', 11, 'maxLength', 11),
+			'message' => 'O CPF deve ter 11 números.',
+			'allowEmpty' => true,
+			'required' => false,
+			//'last' => false, // Stop validation after this rule
+			'on' => 'null'
+		),
+		'rg' => array(
+			'rule' => array('minLength', 9, 'maxLength', 9),
+			'message' => 'O RG deve ter 9 números.',
+			'allowEmpty' => true,
+			'required' => false,
+			//'last' => false, // Stop validation after this rule
+			'on' => 'null'
+		),
    );
 
 }
