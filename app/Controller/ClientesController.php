8c63a22c08bb434767e2055aae0ef2a3ff54faf7
guircs@poli.ufrj.br <guircs@poli.ufrj.br@7746d04f-d786-4d59-b84b-d35678cca735>
2014-12-04 17:49:13 +0000

    Alterados os views de Funcionario e Cliente e o edit de Cliente

--- app/Controller/ClientesController.php
+++ app/Controller/ClientesController.php
@@ -24,16 +24,7 @@ class ClientesController extends AppController {
         if ($this->request->is('post')) {
 
             $this->Cliente->create();
-	    /*$c_nome = $this->request->data['Cliente']['nome'];
-            $c_data = $this->request->data['Cliente']['dataNascimento'];
-            $c_mail= $this->request->data['Cliente']['email'];
-            $c_tel= $this->request->data['Cliente']['telefone'];
-	    $c_dataCadastro = $this->request->data['Cliente']['dataCadastro'];
-	    $c_chaveAntiga = $this->request->data['Cliente']['chaveAntiga'];
-	    $c_cor = $this->request->data['Cliente']['cor'];
-	    $c_genero = $this->request->data['Cliente']['genero'];
-	    $c_nomeSocial = $this->request->data['Cliente']['nomeSocial'];
-	    $c_estadoCivil = $this->request->data['Cliente']['estadoCivil'];*/
+
             //$this->Cliente->set('dataCadastro', $c_dataCadastro);
             $c_save = $this->Cliente->save($this->request->data);
 
